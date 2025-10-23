ee13cc35fa903ee757fad0832134b1cfbab4be26
Mateus <mfnogueira.nogueira@gmail.com>
2025-10-23 11:29:01 -0400

    Added the files of the array iteration class

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,17 @@
+const breakfastMenu = ['Pancakes', 'Eggs and Bacon', 'Cereal', 'Yougurt'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Grilled Chicken', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('mainCourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for(let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem
